6716701cf9c7f09cf0f85f0e979f08344a9c3a84
Joshua James <burke.joshua.james@gmail.com>
2017-09-14 04:22:20 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
   move = board[index]
-  return true unless valid?(move) || index < 0
+  return false if valid?(move) || index < 0
   true
 end
 
